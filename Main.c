1bab1b13191e3c85a40a1dc4665e1cbf61b8585e
Ethan Anderson <ethan.anderson995@gmail.com>
2017-12-06 20:55:11 -0500

    Partner's work

--- Main.c
+++ Main.c
@@ -1,1 +1,30 @@
-main file wow
+//Gabriel Lluch and Ethan Anderson
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void Producer();
+void Consumer();
+int main() {
+
+//create TURN.txt file and initialize to Producer's turn
+  FILE *TURN = fopen("TURN.txt", "w+");
+  if (TURN == NULL) { exit(1); }
+  fputc('0', TURN);
+  fclose(TURN);
+//Create DATA.txt
+  FILE *DATA = fopen("DATA.txt", "w");
+  if (DATA == NULL) { exit(1); }      
+  fclose(DATA);
+
+//fork Producer as child and Consumer as parent
+  int pid;
+  pid = fork();
+
+  if (pid == -1) exit(1);
+  if (pid == 0) Producer();
+  if (pid != 0) Consumer();
+
+  return 0;
+}
+
--- Producer.c
+++ Producer.c
@@ -1,1 +1,31 @@
-producer file
+//PRODUCER, Gabriel Lluch and Ethan Anderson
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void Producer() {
+
+  FILE *mydata = fopen("mydata.txt", "r");
+  char transfer;  
+//continue while until end of file
+  while(!feof(mydata)) {
+    
+//pull turn digit from file
+    FILE *myTURN = fopen("TURN.txt", "r+");
+    if (myTURN == NULL) exit(1);
+    char checkTurn = getc(myTURN);
+    fclose(myTURN);
+//check if my turn, if so then pull char from mydata and push to DATA    
+    if (checkTurn == '0') {
+      transfer = getc(mydata);
+      FILE *DATA = fopen("DATA.txt", "r+");
+      putc(transfer, DATA);
+      fclose(DATA);
+//set to consumer's turn     
+      FILE *yourTURN = fopen("TURN.txt", "r+");
+      putc('1', yourTURN);
+      fclose(yourTURN);
+    }
+  }
+  fclose(mydata);
+}
