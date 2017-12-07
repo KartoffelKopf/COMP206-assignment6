470bad835d5f9bc48d234066ccdcd9cdf6ae1809
Ethan Anderson <ethan.anderson995@gmail.com>
2017-12-06 20:56:16 -0500

    My work

--- Consumer.c
+++ Consumer.c
@@ -1,1 +1,34 @@
-Consumer file
+//Ethan Anderson and Gabriel Lluch
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void Consumer()
+{
+	while(1)
+	{
+		FILE *myturn = fopen("TURN.txt", "r+");
+		char check = getc(myturn);
+		fclose(myturn);
+		
+		/*checks if it is the consumer's turn*/
+		if(check == '1')
+		{
+			FILE *data = fopen("DATA.txt","r");
+			char input = getc(data);
+			
+			if(input == EOF)
+			{
+				puts("");
+				break;
+			}
+	
+			printf("%c", input);
+			fflush(stdout);
+			FILE *yourturn = fopen("TURN.txt", "r+");
+			putc('0', yourturn);
+			fclose(yourturn);
+		}
+	}
+exit(0);
+}
